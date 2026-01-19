d1cca7b514a0c7d19b6c5e8369e1e16aadcbb9be
rlauff <r.lauff1997@gmail.com>
2026-01-19 18:53:40 +0100

    setup basic structure. We will use the ropey crate to handle the large strings and regex crate to handle regex matching

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use ropey::Rope;
+
+
+struct Interpreter {
+    state: Rope,
+    registers: Vec<Vec<String>>,    // the registers
+    idx: usize,     // the index to theplace the interpreter is reading atm
 }
+
+
+fn main() {
+    // create the interpreter
+
+    loop {
+        // parse to find the next scope
+
+        // evaluate it
+    }
+}
+
+fn find_next_scope() {
+    unimplemented!()
+}
+
+fn evaluate_scope() {
+    unimplemented!()
+}
+
+
+
+
