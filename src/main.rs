2c00e9bd818297d409130c21210dd75416050304
rlauff <r.lauff1997@gmail.com>
2026-01-21 21:42:24 +0100

    reworked functions removed def* etc a function now replaces its full input by its result. The input can be (for example) ^$0 to replace the parent scope of the function call by its result
    corrected add_positive
    the pattern (\d*)(\d?) was bad because the (\d?) might not match because (\d*) catches everything
    replaced by (\d*)(\d) to catch the last digit robustly
    the case of empty summands is catched by different arms

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
-use ropey::Rope;
+
+extern crate ropey;
+
+use std::fs::File;
+use std::io;
+
+use ropey::{iter::Chars, Rope, RopeSlice};
 
 
 struct Interpreter {
@@ -10,12 +16,29 @@ struct Interpreter {
 
 fn main() {
     // create the interpreter
+    let filepath = std::env::args().nth(1).expect("Usage: pass path to a file");
+    let mut i = Interpreter{ 
+        state: Rope::from_reader(
+                io::BufReader::new(
+                File::open(&filepath)
+                .unwrap()))
+                .expect("Cannot read file: either it doesn't exist, file permissions don't allow reading, or is not utf8 text."),
+        registers: vec![vec![]],
+        idx: 0,
 
+     };
+
+    let mut change_made = false;
     loop {
+        change_made = false;
         // parse to find the next scope
 
         // evaluate it
+
+        // repeat
+        if !change_made { break }
     }
+    println!("\n\nFinal state:\n{}", i.state);
 }
 
 fn find_next_scope() {
