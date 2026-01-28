1bec5f0d5ea0ee8a7e050b958118df096fdf6202
rlauff <r.lauff1997@gmail.com>
2026-01-28 16:35:43 +0100

    worked on the parsing for finding a scope
    Finish by writing a function that takes the raw scope string { ... } and converts it into a scope object

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,23 @@ extern crate ropey;
 
 use std::fs::File;
 use std::io;
+use std::collections::HashMap;
 
+use regex::Regex;
 use ropey::{iter::Chars, Rope, RopeSlice};
 
-
+struct Function {
+    name: String,           // the name of the function
+    patterns: Vec<Regex>,   // the patterns of the arms to match agains, note that the regexes are precompiled
+    outputs: Vec<Regex>,    // the outputs of the arms
+}
 struct Interpreter {
     state: Rope,
-    registers: Vec<Vec<String>>,    // the registers
-    idx: usize,     // the index to theplace the interpreter is reading atm
+    function_table: HashMap<String, Function> ,     // the function table linking names to the Function object
+    registers: Vec<Vec<String>>,                    // the registers
+    idx: usize,                                     // the index to theplace the interpreter is reading atm
+    verbose: bool,                                  // whether to print out debug info
+    logging: bool,                                  // whether to log to a log file
 }
 
 
@@ -23,8 +32,11 @@ fn main() {
                 File::open(&filepath)
                 .unwrap()))
                 .expect("Cannot read file: either it doesn't exist, file permissions don't allow reading, or is not utf8 text."),
+        function_table: HashMap::new(),
         registers: vec![vec![]],
         idx: 0,
+        verbose: false,
+        logging: false,
 
      };
 
@@ -41,8 +53,67 @@ fn main() {
     println!("\n\nFinal state:\n{}", i.state);
 }
 
-fn find_next_scope() {
-    unimplemented!()
+struct Scope<'a> {
+    start_idx: usize,
+    end_idx: usize,
+    input: RopeSlice<'a>,
+    patterns: Vec<Regex>,
+    outputs: Vec<Regex>,
+}
+enum FindingScopeError {
+    FoundEndingBraceBeforeStartingBrace(String),
+    NoEndingBrace(String),
+    MalformedOrMissingInput(String),
+    MalformedOrMissingPattern(String),
+    MalformedOrMissingOutput(String),
+    ArmsNotSeparatedBySemicolon(String),
+}
+
+/// searches the given rope starting at start_idx to find the next matching {} pair or function call
+fn find_next_scope(i: &Interpreter,r: &Rope, start_idx: usize) -> Result<Option<Scope>, FindingScopeError> {
+    for c in r.chars_at(start_idx) {
+        match c {
+            '{' => {    // found the start of a scope
+                let mut brace_count = 1;
+                let mut end_idx = start_idx + 1;
+                for c2 in r.chars_at(end_idx) {
+                    match c2 {
+                        '{' => brace_count += 1,
+                        '}' => {
+                            brace_count -= 1;
+                            if brace_count == 0 {   // found the end of the scope
+                                // build the scope object
+                                // TODO
+                            }
+                        }
+                        _ => (),   // continue searching
+                        }
+                        end_idx += 1;
+                    }
+                    // if this for loop ends, we have not found a closing brace matching the opening one
+                    match i.verbose {
+                        true => return Err(FindingScopeError::NoEndingBrace(
+                            format!("No matching closing brace found when searching for a scope in {}", 
+                                r.chars_at(start_idx).as_str()))),
+                        false => return Err(FindingScopeError::NoEndingBrace(
+                            "No matching closing brace found when searching for a scope."
+                            .to_string())),
+                    }
+                }
+            '}' => {    // found a closing brace before an opening one
+                match i.verbose {
+                    true => return Err(FindingScopeError::FoundEndingBraceBeforeStartingBrace(
+                        format!("Encountered a closing brace without a matching opening brace when searching for a scope in {}", 
+                            r.chars_at(start_idx).as_str()))),
+                    false => return Err(FindingScopeError::FoundEndingBraceBeforeStartingBrace(
+                        "Encountered a closing brace without a matching opening brace when searching for a scope."
+                        .to_string())),
+                }
+            }
+            _ => (),    // continue searching
+        }
+    }
+    Ok(None)    // did not find any opening or closing braces -> there are no more scopes
 }
 
 fn evaluate_scope() {
